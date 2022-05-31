899934ab78eae894d90f523e5ef9dfb8dc87bfde
Narumikazuchi <ganzel.davy@swschwedt.de>
2022-05-31 13:29:41 +0200

    Fixed error in transforming the decimal.

--- Narumikazuchi.Serialization.Bytes/Serialization Strategies/Decimal.cs
+++ Narumikazuchi.Serialization.Bytes/Serialization Strategies/Decimal.cs
@@ -21,16 +21,26 @@ partial class IntegratedSerializationStrategies
 
     partial struct Decimal : IDeserializationStrategy<System.Byte[], System.Decimal>
     {
-        System.Decimal IDeserializationStrategy<System.Byte[], System.Decimal>.Deserialize(System.Byte[] input) =>
-            new(input.Select(x => (System.Int32)x)
-                     .ToArray());
+        System.Decimal IDeserializationStrategy<System.Byte[], System.Decimal>.Deserialize(System.Byte[] input)
+        {
+            Span<System.Byte> data = input.AsSpan();
+
+            return new(new System.Int32[]
+            {
+                BitConverter.ToInt32(data[..4]),
+                BitConverter.ToInt32(data[4..8]),
+                BitConverter.ToInt32(data[8..12]),
+                BitConverter.ToInt32(data[12..])
+            });
+        }
     }
 
     partial struct Decimal : ISerializationStrategy<System.Byte[], System.Decimal>
     {
         System.Byte[] ISerializationStrategy<System.Byte[], System.Decimal>.Serialize(System.Decimal input) =>
             System.Decimal.GetBits(input)
-                          .Select(x => (System.Byte)x)
+                          .Select(BitConverter.GetBytes)
+                          .SelectMany(x => x)
                           .ToArray();
     }
 }
