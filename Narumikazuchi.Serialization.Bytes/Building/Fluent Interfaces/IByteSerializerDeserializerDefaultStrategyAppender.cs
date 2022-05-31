82bb81f3f6f6b528a6e385e3354e1d5a9c9acc04
Narumikazuchi <ganzel.davy@swschwedt.de>
2022-05-31 16:04:58 +0200

    Updated description for the integrated default strategies.

--- Narumikazuchi.Serialization.Bytes/Building/Fluent Interfaces/IByteSerializerDeserializerDefaultStrategyAppender.cs
+++ Narumikazuchi.Serialization.Bytes/Building/Fluent Interfaces/IByteSerializerDeserializerDefaultStrategyAppender.cs
@@ -15,9 +15,9 @@ public interface IByteSerializerDeserializerDefaultStrategyAppender<TSerializabl
     /// Default strategies will not override any prior custom strategies that have been configured. Furthermore every strategy targeting one
     /// of the types that the default strategies target will overwrite the default strategy with the custom one.<para/>
     /// The default strategies include strategies for the following types: <see cref="Boolean"/>, <see cref="Byte"/>, <see cref="Char"/>,
-    /// <see cref="Double"/>, <see cref="Int16"/>, <see cref="Int32"/>, <see cref="Int64"/>, <see cref="IntPtr"/>, <see cref="SByte"/>,
-    /// <see cref="Single"/>, <see cref="UInt16"/>, <see cref="UInt32"/>, <see cref="UInt64"/>, <see cref="UIntPtr"/>, <see cref="DateTime"/>,
-    /// <see cref="DateOnly"/>, <see cref="TimeOnly"/>, <see cref="TimeSpan"/>, <see cref="Guid"/>, <see cref="Half"/>, <see cref="String"/>.
+    /// <see cref="DateOnly"/>, <see cref="DateTime"/>, <see cref="Decimal"/>, <see cref="Double"/>, <see cref="Guid"/>, <see cref="Half"/>,
+    /// <see cref="Int16"/>, <see cref="Int32"/>, <see cref="Int64"/>, <see cref="IntPtr"/>, <see cref="SByte"/>, <see cref="Single"/>,
+    /// <see cref="String"/>, <see cref="TimeOnly"/>, <see cref="TimeSpan"/>, <see cref="UInt16"/>, <see cref="UInt32"/>, <see cref="UInt64"/>, <see cref="UIntPtr"/>.
     /// </remarks>
     public IByteSerializerDeserializerStrategyAppenderOrFinalizer<TSerializable> UseDefaultStrategies();
 }
